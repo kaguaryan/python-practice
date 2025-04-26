6083d6c4291696be7f8629256b7d726a65336ae0
Ryan <ryankagua@gmail.com>
2025-04-26 20:59:13 +0300

    sum_list

---
+++ sum_list.py
@@ -0,0 +1,7 @@
+def sum_list(numbers):
+    total = 0  # Initialize total
+    for num in numbers:  # Loop through each element
+        total += num  
+    return total  # Return the sum
+
+print(sum_list([10, 20, 30, 40, 50, 60, 70, 80 ]))
